dfc6f11ea8165eb2511fd8d07e278569be9844e5
Jacob Morse <jacobsmorse@gmail.com>
2022-02-11 10:25:18 -0700

    Sdlc corrections

--- sldc.js
+++ sldc.js
@@ -19,8 +19,8 @@
     The first one is done for you. 
 */
 const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
-const analysis of requirements = "This second step involves getting input from all stakeholders. Evaluate the strengths and weaknesses of the current system and determine the cost and resources required to implement the planned improvements."
+const analysisofRequirements = "This second step involves getting input from all stakeholders. Evaluate the strengths and weaknesses of the current system and determine the cost and resources required to implement the planned improvements."
 const design = "This third step starts by turning the software specifications into a design plan. All stakeholders then review this plan and provide feedback and suggestions."
 const implementation = "The fourth step includes user notification and training, installation of hardware and software into the productions environment and integration into the daily processes."
-const testing and integration = "The fifth step involves combining and testing individual software modules as a group. It is done to evaluate the compliance of a system or component with specific functional requirements."
+const TestingandIntegration = "The fifth step involves combining and testing individual software modules as a group. It is done to evaluate the compliance of a system or component with specific functional requirements."
 const maintain = "The last step is the process of modifying a software product after it has been delivered to the customer. This involves software upgrades, repairs and fixes of the broken software."
